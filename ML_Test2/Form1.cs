ce17b9aaad9148db30a8197104a75f530ce18359
mkalme <mikelis.kalme@gmail.com>
2025-05-22 15:08:10 +0300

    Add project files.

---
+++ ML_Test2/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace ML_Test2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            Debug.WriteLine(random.Next(90));
+        }
+    }
+}
